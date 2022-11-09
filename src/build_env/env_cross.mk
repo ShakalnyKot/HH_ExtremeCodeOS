5526a0689ac0c0c29cd57c9fc1fc85429d7cc49a
ShakalnyKot <103893968+ShakalnyKot@users.noreply.github.com>
2022-11-10 01:47:42 +1000

    tweaked env_cross.mk

--- src/build_env/env_cross.mk
+++ src/build_env/env_cross.mk
@@ -4,11 +4,11 @@ ARCH=i386
 TARGET=$(ARCH)-elf
 WORDLENGTH=32
 
-LINK_HH=$(CROSS_BIN)/$(TARGET)-ld -m elf_$(ARCH) -T src/arch/$(ARCH)/hyperhal/link.ld -o
+LINK_HH=i686-linux-gnu-ld -m elf_$(ARCH) -T src/arch/$(ARCH)/hyperhal/link.ld -o
 
 PY=python3
-CC=$(CROSS_BIN)/$(TARGET)-gcc
-CPP=$(CROSS_BIN)/$(TARGET)-g++
+CC=i686-linux-gnu-gcc
+CPP=i686-linux-gnu-g++
 ASM=nasm
 
 CFLAGS=-O0 -Wall -Isrc/include -fno-builtin -ffreestanding -nostdinc -nostdlib -mgeneral-regs-only -c	# compiler arguments
